f2c3de0130d3d5054f1adbac5a15ff3c442147b7
hoanglong123000 <hoanglong123000@gmail.com>
2022-03-29 23:35:29 +0700

    Update Front End on 3/29/2022

---
+++ src/main/java/com/nhhl/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.nhhl.Controller;
+
+import org.springframework.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController
+{
+	@RequestMapping(value= {"/", "/trang-chu"}, method = RequestMethod.GET)
+	public String Main()
+	{
+		return "user/main";
+	}
+}
